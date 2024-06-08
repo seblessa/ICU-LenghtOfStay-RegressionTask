44d53ab925680f7c6ab93b336d5cabe18f6beb93
seblessa <sebastiaolessa2002@outlook.com>
2024-06-09 00:51:54 +0100

    update

--- ReadME.md
+++ ReadME.md
@@ -1,13 +1,44 @@
-# Performance Analysis
+# AlphaZero
+Assignment for Laboratory of Artificial Intelligence and Data Science Class, 3º Year,2º Semester, Bachelor in Artificial Intelligence and Data Science Project 2 – Develop an Alpha Zero Ataxx Player
+
+# Summary
+
+In this project our goal is to implement an AlphaZero player for the game Ataxx and for the game Go.
+
+The AlphaZero algorithm is a reinforcement learning algorithm
+that uses a neural network to approximate the value function and the policy function.
+The neural network is trained using self-play and Monte Carlo Tree Search.
+
+**Autores**:
+- [Sebastião Santos Lessa](https://github.com/seblessa/)
+- [Margarida Vila Chã](https://github.com/margaridavc/)
+
+# Versões
+
+The versions of the operating systems used to develop and test this application are:
+- Fedora 38
+- macOS Sonoma 14.0
+
+Python Versions:
+- 3.11.9
 
 
-Enunciado: https://moodle2324.up.pt/pluginfile.php/225489/mod_label/intro/big.html
+# Requirements
 
+To keep everything organized and simple, we will use MiniConda to manage our environments.
 
- Bachelor in Artificial Intelligence and Data Science. 
- 
+To create an environment with the required packages for this project, run the following commands:
 
-
+```bash
+conda create -n cdle python
 conda install -c conda-forge openjdk=11
+```
+To install the requirements run:
 
-pip install pyspark
+```bash
+pip install -r requirements.txt
+```
+
+# Report
+
+You can see the notebook here: [notebook.ipynb](notebook.ipynb).
--- utils.py
+++ utils.py
@@ -3,40 +3,33 @@ import matplotlib.pyplot as plt
 
 
 def print_missing_value_counts(df):
-    # Calculate null counts for each column
     null_counts = df.select([F.count(F.when(F.isnull(c), c)).alias(c) for c in df.columns])
-
-    # To display in a more readable vertical format
     null_counts.show(vertical=True)
 
 
 def mean_residuals(predictions, actuals):
-    # Ensure predictions and actuals are columns of DataFrames
     df = predictions.join(actuals)
-    df = df.withColumn('Residual', F.col('prediction') - F.col('LOS'))  # Ensure correct column names are used
-
+    df = df.withColumn('Residual', F.col('prediction') - F.col('LOS'))
     # Calculate mean of residuals
     mean_residual = df.select(F.avg('Residual')).first()[0]
 
-    return abs(round(mean_residual, 2))
+    return abs(round(mean_residual, 5))
 
 
 def print_metrics(evaluator, predictions):
-    # Mean of differences between predicted and actual values
     mean_residual = mean_residuals(predictions.select("prediction"), predictions.select("LOS"))
     r2 = evaluator.evaluate(predictions, {evaluator.metricName: "r2"})
     rmse = evaluator.evaluate(predictions, {evaluator.metricName: "rmse"})
     mse = evaluator.evaluate(predictions, {evaluator.metricName: "mse"})
 
     print(
-        f"Mean of the difference between predicted and actual 'LOS': {mean_residual}.\n"
-        f"R2 score (proportion of variance predictable): {r2:.2f}.\n"
-        f"Root Mean Squared Error (average magnitude of errors): {rmse:.2f}.\n"
-        f"Mean Squared Error (average of the squares of errors): {mse:.2f}.\n"
+        f"Mean of the difference between predicted and actual 'LOS': {mean_residual}\n"
+        f"R2 score (proportion of variance predictable): {r2:.2f}\n"
+        f"Root Mean Squared Error (average magnitude of errors): {rmse:.2f}\n"
+        f"Mean Squared Error (average of the squares of errors): {mse:.2f}\n"
     )
 
 
-# Define the UDF
 def handle_list(value):
     if isinstance(value, list):
         if len(value) == 1:
@@ -49,30 +42,24 @@ def transform_list(value_list):
         return None
     unique_values = set(value_list)
     if len(unique_values) == 1:
-        # Return the single element wrapped in an array
         return list(unique_values)
-    return value_list  # Return the original list if values are not the same
+    return value_list
 
 
-# Define the UDF to replace empty lists with 'UNKNOWN'
 def replace_empty_list(value_list):
-    if not value_list:  # Checks if the list is empty
+    if not value_list:
         return ["UNKNOWN"]
     return value_list
 
 
 def plot_graph(df, group_by_column, aggregate_column, agg_func, plot_title, x_label, y_label):
-    # Aggregate the datasets
     aggregated_data = df.groupBy(group_by_column).agg(agg_func(aggregate_column).alias('agg_result')).collect()
 
-    # Extracting results into lists for plotting
     categories = [row[group_by_column] for row in aggregated_data]
     values = [row['agg_result'] for row in aggregated_data]
 
-    # Choose random colors for the bars
     colors = [plt.cm.tab10(i / len(categories)) for i in range(len(categories))]
 
-    # Plotting the bar chart
     plt.figure(figsize=(10, 6))
     plt.bar(categories, values, color=colors, alpha=0.7)
     plt.title(plot_title)
